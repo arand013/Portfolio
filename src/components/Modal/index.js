388419827cf3b234e44fc43c5c5373cef2204332
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-09-06 03:16:30 -0700

    component: Modal: index.js created

---
+++ src/components/Modal/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Modal = ({ onClose, currentPhoto }) => {
+    const { name, description, category, index  } = currentPhoto;
+  
+    
+  return (
+
+    <div>
+        
+    </div>
+    );
+};
+    export default Modal;
