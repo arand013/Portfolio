d8b76361ef41527d889e538d71a00286d28ea47e
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-09-06 03:11:38 -0700

    components: photolist: update

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,8 +5,14 @@ function Nav(props) {
   const {
     categories = [],
     setCurrentCategory,
+    contactSelected,
     currentCategory,
+    setContactSelected,
   } = props;
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">
@@ -18,21 +24,24 @@ function Nav(props) {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a
-              href="#about"
-            >
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
-          <li>
-            <span>Contact</span>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
-            <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'
-              }`} key={category.name}>
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && !contactSelected && 'navActive'
+                }`}
+              key={category.name}
+            >
               <span
                 onClick={() => {
-                  setCurrentCategory(category)
+                  setCurrentCategory(category);
+                  setContactSelected(false);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 const PhotoList = ({ category }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState();
 
   const [photos] = useState([
     {
@@ -25,6 +27,11 @@ const PhotoList = ({ category }) => {
     },
   ]);
   const currentPhotos = photos.filter((photo) => photo.category === category);
+
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({ ...image, index: i });
+    setIsModalOpen(!isModalOpen);
+  };
 
   return (
     <div>
