4422f047ae1904498bbc9879f15077df60968d30
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-09-05 23:53:07 -0700

    Components: Contact: index.js updated

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,3 +1,18 @@
 import React, { useState } from 'react';
 
 import { validateEmail } from '../../utils/helpers';
+
+function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  
+    const [errorMessage, setErrorMessage] = useState('');
+    const { name, email, message } = formState;
+  
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!errorMessage) {
+          setFormState({ [e.target.name]: e.target.value });
+          console.log('Form', formState);
+        }
+      };
+    
