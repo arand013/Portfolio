59d3209ca2a890eda36df70b825897ef6fbb044e
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-09-06 22:22:18 -0700

    icons added to footer'

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,62 @@
 import React from 'react';
-import { Container, Row, Col } from "react-bootstrap";
 import {
     AiFillGithub,
     AiOutlineTwitter,
     AiFillInstagram,
-  } from "react-icons/ai";
-  import { FaLinkedinIn } from "react-icons/fa";
-  
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
 
 function Footer() {
 
     return (
-        let date = new Date();
-        let year = date.getFullYear();
         <footer>
-        <h2>© 2021 Alexander Aranda. All Rights Reserved.</h2>
-       
-  
-  export default Footer;
-  
+            <h2>© 2021 Alexander Aranda. All Rights Reserved.</h2>
+            
+                <ul className="footer-icons">
+                    <li className="social-icons">
+                        <a
+                            href="https://github.com/arand013"
+                            style={{ color: "white" }}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <AiFillGithub />
+                        </a>
+                    </li>
+                    <li className="social-icons">
+                        <a
+                            href="https://twitter.com/"
+                            style={{ color: "white" }}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <AiOutlineTwitter />
+                        </a>
+                    </li>
+                    <li className="social-icons">
+                        <a
+                            href="https://www.linkedin.com/in/alexander-aranda1/"
+                            style={{ color: "white" }}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FaLinkedinIn />
+                        </a>
+                    </li>
+                    <li className="social-icons">
+                        <a
+                            href="https://www.instagram.com/"
+                            style={{ color: "white" }}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <AiFillInstagram />
+                        </a>
+                    </li>
+                </ul>
+        </footer>
+    );
+}
+
+export default Footer;
