21d8515188a5b5ef0e0b251a4566bb8772f477d1
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-09-06 22:03:21 -0700

    component footer update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import {
+    AiFillGithub,
+    AiOutlineTwitter,
+    AiFillInstagram,
+  } from "react-icons/ai";
+  import { FaLinkedinIn } from "react-icons/fa";
+  
 
 function Footer() {
 
     return (
+        let date = new Date();
+        let year = date.getFullYear();
         <footer>
         <h2>© 2021 Alexander Aranda. All Rights Reserved.</h2>
-    </footer>
-    );
-  }
+       
   
   export default Footer;
   
