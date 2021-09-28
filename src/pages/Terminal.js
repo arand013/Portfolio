2e7491875dc063d8fae0edd203c8be9dc168aa27
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-09-27 20:55:11 -0700

    src: pages: Terminal.js updated

--- src/pages/Terminal.js
+++ src/pages/Terminal.js
@@ -12,3 +12,36 @@ function Terminal(props) {
       props.history.push('/about');
     }, 2500);
   };
+
+  return (
+    <div fluid className="terminal bg-dark">
+      <h1 className="text-secondary display-5 display-md-4">
+        <p><span>&#x203A;</span></p>
+        <span>
+          <Typist
+            avgTypingDelay={100}
+            stdTypingDelay={50}
+            onTypingDone={onTypingDone}
+            startDelay={100}
+            cursor={{
+              blink: true,
+              hideWhenDone: true
+            }}
+          >
+            <p><span>&#x203A;</span></p>
+            <Typist.Delay ms={150}></Typist.Delay>
+            <p><span>&#x203A;</span></p>
+            <Typist.Delay ms={150}></Typist.Delay>
+            <span className="pr-2 pr-md-4">&#x203A;</span>
+            <span className="text-light">meetVanessaLane();</span>
+          </Typist>
+        </span>
+        {showSuccess && 
+          <p className="text-success mt-3">Redirecting...</p>
+        }
+      </h1>
+    </div>
+  )
+}
+
+export default Terminal;
