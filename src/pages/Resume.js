25cb9d4ae82c6d814b87d3c079fc040dc744f6f5
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-09-27 20:53:40 -0700

    src: pages: Resume.js created

---
+++ src/pages/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import react-bootstrap components
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+
+function Resume() {
+
+    
+}
